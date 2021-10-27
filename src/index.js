140123dcfed704c433051797075276c920678266
Atsighi Bright <brytebee@gmail.com>
2021-10-27 18:12:55 +0100

    Fix styling by creating more re-usable css classes & rebuild

--- src/index.js
+++ src/index.js
@@ -24,14 +24,18 @@ const populateRoot = (array) => {
   array.forEach((item) => {
     const ul = document.createElement('ul');
     const li = document.createElement('li');
-    li.innerHTML = `<div class="list">
-  <input type="checkbox" name="check" class="check">
-  <span class="description">${item.description}</span>
-  <span class="options"><i class="fas fa-ellipsis-v"></i></span>
-  <span class="trash hidden"><i class="fa fa-trash" aria-hidden="true"></i></span>
-  <span class="done hidden"><i class="fas fa-check"></i></span>
-  <hr>
-</div>`;
+    li.innerHTML = `
+      <div class="list d-flex parent-flex b-border">
+        <div class="child-flex-large">
+          <input type="checkbox" name="check" class="check">
+          <span class="description">${item.description}</span>
+        </div>
+        <div class="child-flex-small">      
+          <span class="options"><i class="fas fa-ellipsis-v"></i></span>
+          <span class="trash hidden"><i class="fa fa-trash" aria-hidden="true"></i></span>
+          <span class="done hidden"><i class="fas fa-check"></i></span>
+        </div>
+      </div>`;
     ul.append(li);
     root.append(ul);
   });
