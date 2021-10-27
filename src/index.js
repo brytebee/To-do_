4701b7d1d942134d3ef1ef9e3097c770a9365e1d
Atsighi Bright <brytebee@gmail.com>
2021-10-27 21:34:36 +0100

    Add module, create stateChange function, and modify index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import stateChange from './module/stateChange';
 import './style.css';
 
 const root = document.querySelector('#root');
@@ -24,18 +25,58 @@ const populateRoot = (array) => {
   array.forEach((item) => {
     const ul = document.createElement('ul');
     const li = document.createElement('li');
-    li.innerHTML = `
-      <div class="list d-flex parent-flex b-border">
-        <div class="child-flex-large">
-          <input type="checkbox" name="check" class="check">
-          <span class="description">${item.description}</span>
-        </div>
-        <div class="child-flex-small">      
-          <span class="options"><i class="fas fa-ellipsis-v"></i></span>
-          <span class="trash hidden"><i class="fa fa-trash" aria-hidden="true"></i></span>
-          <span class="done hidden"><i class="fas fa-check"></i></span>
-        </div>
-      </div>`;
+
+    const listDiv = document.createElement('div');
+    listDiv.className = 'list d-flex parent-flex b-border';
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.className = 'child-flex-large';
+
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    check.className = 'check';
+    check.addEventListener('change', (e) =>
+      stateChange(e, item.index, item.completed)
+    );
+
+    const descriptionSpan = document.createElement('span');
+    descriptionSpan.className = 'description';
+    descriptionSpan.textContent = item.description;
+
+    const iconsDiv = document.createElement('div');
+    iconsDiv.className = 'child-flex-small';
+
+    const optionsSpan = document.createElement('span');
+    optionsSpan.className = 'options';
+    optionsSpan.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+
+    const trashSpan = document.createElement('span');
+    trashSpan.className = 'trash hidden';
+    trashSpan.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+
+    const taskCompleteSpan = document.createElement('span');
+    taskCompleteSpan.className = 'done hidden';
+    taskCompleteSpan.innerHTML = '<i class="fas fa-check"></i>';
+
+    descriptionDiv.append(check, descriptionSpan);
+    iconsDiv.append(optionsSpan, trashSpan, taskCompleteSpan);
+
+    listDiv.append(descriptionDiv, iconsDiv);
+
+    li.appendChild(listDiv);
+
+    // li.innerHTML = `
+    //   <div class="list d-flex parent-flex b-border">
+    //     <div class="child-flex-large">
+    //       <input type="checkbox" name="check" class="check">
+    //       <span class="description">${item.description}</span>
+    //     </div>
+    //     <div class="child-flex-small">
+    //       <span class="options"><i class="fas fa-ellipsis-v"></i></span>
+    //       <span class="trash hidden"><i class="fa fa-trash" aria-hidden="true"></i></span>
+    //       <span class="done hidden"><i class="fas fa-check"></i></span>
+    //     </div>
+    //   </div>`;
     ul.append(li);
     root.append(ul);
   });
---
+++ src/module/stateChange.js
@@ -0,0 +1,7 @@
+const stateChange = (e, index, state) => {
+  if (e.target.checked) {
+    state = true;
+  }
+};
+
+export default stateChange;
