9b992cfe459133450eace367741ab047dd68b2c9
Atsighi Bright <brytebee@gmail.com>
2021-11-04 11:41:46 +0100

    Install babel, creatw mock folder/file and create test folder/file

---
+++ src/test/addItem.test.js
@@ -0,0 +1,9 @@
+import add from '../addItem.js';
+
+jest.mock('../addItem');
+
+describe('Add or Remove item', () => {
+  test('Add an item to list', () => {
+    expect(add().length).toBe(1);
+  });
+});
