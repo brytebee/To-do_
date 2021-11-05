15c29a1f8141249bbddc8ec07b70547dc401bda9
Oluwafemi <femikola23@gmail.com>
2021-11-05 11:55:35 +0400

    remove linters

--- src/__mocks__/updateIndex.js
+++ src/__mocks__/updateIndex.js
@@ -1,5 +1,5 @@
 const updateIndex = (list) => {
-    document.body.innerHTML = `
+  document.body.innerHTML = `
     <span class="addSpan d-flex parent-flex">
       <input type="text" name="addItem" class="addList double-line-height child-flex-large"
         placeholder="Add to your list...">
@@ -7,23 +7,23 @@ const updateIndex = (list) => {
     </span>
     <div id="root"></div>`;
 
-    list = [];
-    class Item {
-      constructor(description, list) {
-        this.completed = false;
-        this.description = description;
-        this.index = list.length + 1;
-      }
+  list = [];
+  class Item {
+    constructor(description, list) {
+      this.completed = false;
+      this.description = description;
+      this.index = list.length + 1;
     }
+  }
 
-    const input = new Item('one', list);
-    list.push(input.index);
-    const input2 = new Item('two', list);
-    list.push(input2.index);
-    list.pop(); // remove the index = 2
-    const input3 = new Item('three', list);
-    list.push(input3.index);
-    return list;
-  };
+  const input = new Item('one', list);
+  list.push(input.index);
+  const input2 = new Item('two', list);
+  list.push(input2.index);
+  list.pop(); // remove the index = 2
+  const input3 = new Item('three', list);
+  list.push(input3.index);
+  return list;
+};
 
-  export default updateIndex;
+export default updateIndex;
--- src/test/edit.test.js
+++ src/test/edit.test.js
@@ -7,7 +7,7 @@ import editItem from '../edit.js';
 jest.mock('../edit');
 
 describe('Edit function', () => {
-   test('Edit a list item', () => {
-     expect(editItem()).toBe('some state...');
-   });
+  test('Edit a list item', () => {
+    expect(editItem()).toBe('some state...');
+  });
 });
